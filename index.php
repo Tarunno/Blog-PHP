bceb3a9ff1a6fdeeb353b2962abece9ea0cd8a2d
Mahadi Hasan Tarunno <47926003+Tarunno@users.noreply.github.com>
2020-08-16 22:34:45 +0600

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css"/>
         <link rel="stylesheet" href="css/index.css">
-        <title>TARUNNO SAYS</title>
+        <title>TARUNNO SAYS | home</title>
     </head>
     <body>
         <div class="page-wrapper">
