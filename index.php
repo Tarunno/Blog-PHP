5d860903f5bca2e8547543ebda67f5fe025de23d
Mahadi Hasan Tarunno <47926003+Tarunno@users.noreply.github.com>
2020-08-23 02:58:39 +0600

    Update index.php

--- index.php
+++ index.php
@@ -45,48 +45,10 @@
         <div class="content clearify">
             <div class="main-content">
                 <h1 class="recent-post-title"> Recent posts</h1>
-                <?php
-                    $posts = selectAll('posts');
-                    $post_array = array();
-                	while($row = mysqli_fetch_assoc($posts)){
-                		$post_array[] = $row;
-                	}
-                	$posts = array_reverse($post_array);;
-                    foreach($posts as $post){
-                        $user = selectOne('users', ['id' => $post['user_id']]);
-                        $user_name = mysqli_fetch_assoc($user);
-                        if($post['published'] == 1){
-                        echo '<div class="content-post" data-aos="slide-right">
-                            <div class="post-image" style="background-image: linear-gradient(rgba(0, 0, 0, .4), rgba(0, 0, 0, .4)),url(images/'.$post['image'].');
-                                         background-size: cover;
-                                         background-position:center;">
-                            </div>
-                            <div class="post-preview">
-                                <h1> <a href="single.php?id='.$post['id'].'">'.$post['title'].'</a></h1>
-                                <i class="far fa-user"> <p>'.$user_name['username'].'</p></i>
-                                <p style="display: inline; padding:0px 5px 0px 5px;"> | </p>
-                                <i class="far fa-calendar-alt"> <p>'.$post['created_at'].'</p></i>
-                                <p style="display: inline; padding:0px 5px 0px 5px;"> | </p>
-                                <i class="fas fa-paperclip"></i> <p>'.$post['topics'].' | </p></i>
-                                <div class="rating_index" style="display: inline;">
-                                    <i id="1" class="far fa-star"></i>
-                                    <i id="2" class="far fa-star"></i>
-                                    <i id="3" class="far fa-star"></i>
-                                    <i id="4" class="far fa-star"></i>
-                                    <i id="5" class="far fa-star"></i>
-                                </div>';
-                                if($post['rated_users'] == 0){
-                                    echo '<input type="hidden" name="name" class="rating_holder_index" value="'.$post['sum_rating'].'">';
-                                }else{
-                                    echo '<input type="hidden" name="name" class="rating_holder_index" value="'.floor($post['sum_rating'] / $post['rated_users']).'">';
-                                }
-                                echo '<br><br>
-                                <p>'.substr($post['body'], 0, 260).'<a href="single.php?id='.$post['id'].'">...read more</a></p>
-                            </div>
-                        </div>';
-                        }
-                    }
-                ?>
+                    <div class="post-res">
+                        <!--posts render here-->
+                    </div>
+                    <button class="load-more-btn" type="button" onclick="load_more()"> Load more</button>
             </div>
             <div class="side-bar">
                 <div class="section search" data-aos="slide-left">
@@ -111,7 +73,9 @@
         </div>
 
         <?php  include 'includes/footer.inc.php' ?>
+        <script type="text/javascript" src="javaScript/ajax.js"></script>
         <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
         <script type="text/javascript" src="javaScript/app.js"></script>
     </body>
 </html>
+
